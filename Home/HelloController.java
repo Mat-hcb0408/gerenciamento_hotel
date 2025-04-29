76ad8bc841062174924451db62951ca8a2eb77f1
Ryan Gabriel <ryangabrielsilvag052@gmail.com>
2025-04-29 14:30:49 -0300

    Update HelloController.java

--- Home/HelloController.java
+++ Home/HelloController.java
@@ -13,16 +13,30 @@ public class HelloController {
     private ImageView Praia_Imagem;
 
     @FXML
+    private ImageView Logo_Imagem;
+
+    @FXML
     public void initialize() {
-        carregarImagem(); // Chamada ao iniciar a interface
+        carregarImagemFundo(); // Chamada ao iniciar a interface
+        carregarImagemLogo();
     }
 
     @FXML
-    public void carregarImagem() {
+    public void carregarImagemFundo() {
         try {
             String url = "https://classic.exame.com/wp-content/uploads/2024/08/ED1266_FOTOS18.jpg?quality=70&strip=info";
             Image imagem = new Image(url, true); // TRUE para carregar em background
             Praia_Imagem.setImage(imagem);
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar a imagem: " + e.getMessage());
+        }
+    }
+    @FXML
+    public void carregarImagemLogo() {
+        try {
+            String url = "https://raw.githubusercontent.com/Mat-hcb0408/gerenciamento_hotel/refs/heads/main/SkyWalker%20Hot%C3%A9is.png";
+            Image imagem = new Image(url, true); // TRUE para carregar em background
+            Logo_Imagem.setImage(imagem);
         } catch (Exception e) {
             System.out.println("Erro ao carregar a imagem: " + e.getMessage());
         }
