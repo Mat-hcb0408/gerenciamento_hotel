195eac9436fa5327d48980e6b72837bdb4b0544e
David-son-frs <fdavidson991@gmail.com>
2025-05-06 14:40:31 -0300

    Update Controller.java

--- Login/Controller.java
+++ Login/Controller.java
@@ -1,40 +1,29 @@
-package org.example.projeto_hotel;
+package org.example.demo3;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
 
     @FXML
-    private Button entrar;
-    @FXML
-    private Button cadastreSe;
-    @FXML
-    private TextField emailField;
-    @FXML
-    private PasswordField senhaField;
-    @FXML
-    private ImageView Fundo_Praia;
+    private ImageView Logo_Imagem;
 
     @FXML
     public void initialize() {
-        carregarImagem(); // Chamada ao iniciar a interface
+        carregarImagemLogo();
     }
 
-    @FXML
-    public void carregarImagem() {
-        try {
-            String url = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/24/c5/16/rooftop-swimming-pool.jpg?w=1200&h=-1&s=1"; // coloque aqui o link real da imagem
-            Image imagem = new Image(url);
-            Fundo_Praia.setImage(imagem);
-        } catch (Exception e) {
-            System.out.println("Erro ao carregar a imagem: " + e.getMessage());
-        }
+    public void carregarImagemLogo() {
+        new Thread(() -> {
+            try {
+                String url = "https://raw.githubusercontent.com/Mat-hcb0408/gerenciamento_hotel/main/SkyWalker%20Hot%C3%A9is_claro.png";
+                Image imagem = new Image(url, true);
+                Platform.runLater(() -> Logo_Imagem.setImage(imagem));
+            } catch (Exception e) {
+                System.out.println("Erro ao carregar a imagem do logo: " + e.getMessage());
+            }
+        }).start();
     }
-
-    // Outros métodos para manipular a interface de login
 }
